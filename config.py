db5c606dc763314fd54f6b2a1d87856eea64712b
TCMSKhoa <158541300+KhoaAki@users.noreply.github.com>
2024-12-04 13:42:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Tải các biến môi trường từ file .env
+load_dotenv(dotenv_path=".venv/.env")  # Tải các biến môi trường từ file .env
 
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
-#DATABASE_URL = os.getenv("DATABASE_URL")
-ALGORITHM = "HS256"
-CLIENT_ID1 = '4237b0a75e6a75e'
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-AVATAR_UPLOAD_PATH = "C:\\Users\\Hi\\OneDrive\\Tài liệu\\GitHub\\fe_quiz\\public"
-imageprofile = "https://cdn.glitch.global/83bcea06-7c19-41ce-bb52-ae99ba3f0bd0/JaZBMzV14fzRI4vBWG8jymplSUGSGgimkqtJakOV.jpeg?vUB=1728536441877"
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+AVATAR_UPLOAD_PATH = os.getenv("AVATAR_UPLOAD_PATH")
+imageprofile = os.getenv("imageprofile")
+DATABASE_URL = os.getenv("DATABASE_URL")
+SMTP_SERVER= os.getenv("SMTP_SERVER")
+SMTP_PORT= int(os.getenv("SMTP_PORT"))
+SMTP_USER= os.getenv("SMTP_USER")
+SMTP_PASS= os.getenv("SMTP_PASS")
+
